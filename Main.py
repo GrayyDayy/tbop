1b4f3705b77b5e91b79eb7501442e5e842ec1e83
zEMrees44 <edwardcmreese@gmail.com>
2026-05-07 12:48:28 -0400

    Merged sprites branch.

--- Main.py
+++ Main.py
@@ -3,13 +3,17 @@ from Projectiles import Projectile
 import time
 
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode()
 clock = pygame.time.Clock()
 running = True
 dt = 0
 bullets = []
 cooldown = 0.5
 timesinceshot = 0
+facingleft = False
+facingright = False
+facingup = False
+facingdown = True
 
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
@@ -23,46 +27,85 @@ while running:
         bullet.x += bullet.vel_x
         bullet.y += bullet.vel_y
 
-        if not (0 < bullet.x < 1280 and 0 < bullet.y < 720):
+        if not (0 < bullet.x < screen.get_width() and 0 < bullet.y < screen.get_height()):
             bullets.remove(bullet)
 
-    screen.fill("white")
+    screen.blit(pygame.transform.scale(pygame.image.load("sprites/startroom.png"),(screen.get_width(), screen.get_height())), (0, 0), area=screen.get_rect())
 
-    pygame.draw.circle(screen, "green", player_pos, 40)
-    pygame.draw.circle(screen, "blue", player_pos, 20)
-    pygame.draw.circle(screen, "black", player_pos, 10)
+    if facingleft:
+        screen.blit(pygame.image.load("sprites/leftp.png"), (player_pos.x - 75, player_pos.y - 88),
+                    area=screen.get_rect())
+    elif facingright:
+        screen.blit(pygame.image.load("sprites/rightp.png"), (player_pos.x - 75, player_pos.y - 88),
+                    area=screen.get_rect())
+    elif facingup:
+        screen.blit(pygame.image.load("sprites/backp.png"), (player_pos.x - 75, player_pos.y - 88),
+                    area=screen.get_rect())
+    elif facingdown:
+        screen.blit(pygame.image.load("sprites/frontp.png"), (player_pos.x - 75, player_pos.y - 88),
+                    area=screen.get_rect())
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
+        facingleft = False
+        facingright = False
+        facingup = True
+        facingdown = False
         player_pos.y -= 300 * dt
     if keys[pygame.K_s]:
+        facingleft = False
+        facingright = False
+        facingup = False
+        facingdown = True
         player_pos.y += 300 * dt
     if keys[pygame.K_a]:
+        facingleft = True
+        facingright = False
+        facingup = False
+        facingdown = False
         player_pos.x -= 300 * dt
     if keys[pygame.K_d]:
+        facingleft = False
+        facingright = True
+        facingup = False
+        facingdown = False
         player_pos.x += 300 * dt
     if keys[pygame.K_UP]:
+        facingleft = False
+        facingright = False
+        facingup = True
+        facingdown = False
         if currenttime - timesinceshot > cooldown:
             timesinceshot = currenttime
             if len(bullets) < 6:
-                bullets.append(Projectile(player_pos.x,player_pos.y,12,"blue",0,-5))
+                bullets.append(Projectile(player_pos.x,player_pos.y,12,"blue",0,-8))
     if keys[pygame.K_DOWN]:
+        facingleft = False
+        facingright = False
+        facingup = False
+        facingdown = True
         if currenttime - timesinceshot > cooldown:
             timesinceshot = currenttime
             if len(bullets) < 6:
-                bullets.append(Projectile(player_pos.x,player_pos.y,12,"blue",0,5))
+                bullets.append(Projectile(player_pos.x,player_pos.y,12,"blue",0,8))
     if keys[pygame.K_LEFT]:
+        facingleft = True
+        facingright = False
+        facingup = False
+        facingdown = False
         if currenttime - timesinceshot > cooldown:
             timesinceshot = currenttime
             if len(bullets) < 6:
-                bullets.append(Projectile(player_pos.x,player_pos.y,12,"blue",-5,0))
+                bullets.append(Projectile(player_pos.x,player_pos.y,12,"blue",-8,0))
     if keys[pygame.K_RIGHT]:
+        facingleft = False
+        facingright = True
+        facingup = False
+        facingdown = False
         if currenttime - timesinceshot > cooldown:
             timesinceshot = currenttime
             if len(bullets) < 6:
-                bullets.append(Projectile(player_pos.x,player_pos.y,12,"blue",5,0))
-
-
+                bullets.append(Projectile(player_pos.x,player_pos.y,12,"blue",8,0))
 
     for bullet in bullets:
         bullet.draw(screen)
